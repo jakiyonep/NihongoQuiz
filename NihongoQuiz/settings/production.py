c692ad218425076cb9481ef919edc8333733bb38
jakiyonep <akiyone0820@gmail.com>
2020-05-26 20:24:59 +0900

    a

--- NihongoQuiz/settings/production.py
+++ NihongoQuiz/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import dj_database_url
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -7,3 +8,20 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'name',
+        'USER': 'user',
+        'PASSWORD': '',
+        'HOST': 'host',
+        'PORT': '',
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
+
+import django_heroku
+django_heroku.settings(locals())
