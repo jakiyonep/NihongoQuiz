3f5bb92cdd56a3a8bfc3c3086509780d7d01793b
jakiyonep <akiyone0820@gmail.com>
2020-08-04 08:25:16 +0900

    a

--- quiz/models.py
+++ quiz/models.py
@@ -11,11 +11,80 @@ from django.contrib.auth.models import UserManager, AbstractBaseUser, Permission
 # Create your models here.
 # BeforeYouStart
 
-""""
-class User(AbstractUser):
+
+class CustomUserManager(UserManager):
+    """ユーザーマネージャー"""
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('メールアドレスを入力してください')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """カスタムユーザーモデル."""
+
+    email = models.EmailField(_('メールアドレス'), unique=True)
+    nickname = models.CharField(_('ニックネーム'), max_length=150, blank=True, unique=True)
+
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_(
+            'Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = CustomUserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     class Meta:
-        db_table = 'auth_user'
-"""
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    @property
+    def username(self):
+        """username属性のゲッター
+
+        他アプリケーションが、username属性にアクセスした場合に備えて定義
+        メールアドレスを返す
+        """
+        return self.email
 
 
 
