7041bffc3bca7fd8616c63f74543e695aaaefedf
unknown <akt.handbal0820@gmail.com>
2021-01-30 16:44:40 +0900

    a

--- NihongoQuiz/settings/production.py
+++ NihongoQuiz/settings/production.py
@@ -5,7 +5,7 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
